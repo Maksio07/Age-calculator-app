37937238ca93e9633c26d42539aa65d19322e7a1
Maks <mmaksimov20000806@gmail.com>
2023-09-19 21:50:52 +0200

    added variables in saas

--- js/main.js
+++ js/main.js
@@ -20,16 +20,16 @@ const main = () => {
 }
 
 const prepareDOMElements = () => {
-	$dayInput = document.querySelector('.app-body__inputs__box--input-day')
-	$monthInput = document.querySelector('.app-body__inputs__box--input-month')
-	$yearInput = document.querySelector('.app-body__inputs__box--input-year')
-	$yearResult = document.querySelector('.app-body__results__box--result-years')
-	$monthResult = document.querySelector('.app-body__results__box--result-months')
-	$dayResult = document.querySelector('.app-body__results__box--result-days')
+	$dayInput = document.querySelector('.app-body__inputs__box-input--day')
+	$monthInput = document.querySelector('.app-body__inputs__box-input--month')
+	$yearInput = document.querySelector('.app-body__inputs__box-input--year')
+	$yearResult = document.querySelector('.app-body__results__box-result--years')
+	$monthResult = document.querySelector('.app-body__results__box-result--months')
+	$dayResult = document.querySelector('.app-body__results__box-result--days')
 	$countBtn = document.querySelector('.app-body__button--button')
-	$dayErrorText = document.querySelector('.app-body__inputs__box--error-day')
-	$monthErrorText = document.querySelector('.app-body__inputs__box--error-month')
-	$yearErrorText = document.querySelector('.app-body__inputs__box--error-year')
+	$dayErrorText = document.querySelector('.app-body__inputs__box-error--day')
+	$monthErrorText = document.querySelector('.app-body__inputs__box-error--month')
+	$yearErrorText = document.querySelector('.app-body__inputs__box-error--year')
 	$inputsSection = document.querySelector('.app-body__inputs')
 	$date = new Date()
 	$currentYear = $date.getFullYear()
@@ -42,55 +42,81 @@ const prepareDOMEvents = () => {
 }
 
 const daysInMonth = (month, year) => {
-	return new Date(year, month, 0).getDate();
+	return new Date(year, month, 0).getDate()
 }
 
-const checkInputs = () => {
+const clearOutputs = () => {
+	$dayResult.textContent = '--'
+	$monthResult.textContent =  '--'
+	$yearResult.textContent = '--'
+}
 
+// check inputs
+
+const checkInputs = () => {
 	if ($dayInput.value > daysInMonth($monthInput.value, $yearInput.value)) {
-		$inputsSection.classList.add('error');
-		$dayErrorText.textContent = 'Must be a valid day'
+		$inputsSection.classList.add('error')
+		$dayErrorText.textContent = 'Must be a valid date'
 		$monthErrorText.textContent = ''
 		$yearErrorText.textContent = ''
+		clearOutputs();
 	} else if ($monthInput.value > 12) {
-		$inputsSection.classList.add('error');
+		$inputsSection.classList.add('error')
 		$monthErrorText.textContent = 'Must be a valid month'
 		$dayErrorText.textContent = ''
 		$yearErrorText.textContent = ''
-	} else if ($yearInput.value > $currentYear || $yearInput.value === $currentYear) {
-		$inputsSection.classList.add('error');
+		clearOutputs();
+	} else if ($yearInput.value > $currentYear) {
+		$inputsSection.classList.add('error')
 		$yearErrorText.textContent = 'Must be in the past'
 		$dayErrorText.textContent = ''
 		$monthErrorText.textContent = ''
+		clearOutputs();
+	} else if (
+		($dayInput.value > $currentDay && $monthInput.value > $currentMonth && $yearInput.value == $currentYear) ||
+		($monthInput.value > $currentMonth && $yearInput.value == $currentYear)
+	) {
+		$inputsSection.classList.add('error')
+		$dayErrorText.textContent = 'Must be a valid day'
+		$monthErrorText.textContent = 'Must be a valid month'
+		$yearErrorText.textContent = 'Must be in the past'
+		clearOutputs()
 	} else if ($dayInput.value === '' || $monthInput.value === '' || $yearInput.value === '') {
-		$inputsSection.classList.add('error'); 
+		$inputsSection.classList.add('error')
+		clearOutputs();
 	} else {
-		$inputsSection.classList.remove('error');
+		$inputsSection.classList.remove('error')
 		$inputsSection.firstElementChild.nextElementSibling.classList.remove('error')
 		countAge()
 	}
 }
+
+// calculate outpoots
 
 const countAge = () => {
 	const countedYear = $currentYear - $yearInput.value
 	const countedMonth = $currentMonth + 1 - $monthInput.value
 	const countedDays = $currentDay - $dayInput.value
 
-	if (countedMonth < 0 && countedDays < 0) {
-		$yearResult.textContent = `${countedYear - 1}`
-		$monthResult.textContent = `${12 - -countedMonth}`
-		$dayResult.textContent = `${daysInMonth($monthInput.value, $yearInput.value) - -countedDays}`
+	if (countedDays < 0 && countedMonth < 0) {
+		$dayResult.textContent = daysInMonth($monthInput.value, $yearInput.value) + countedDays
+		$monthResult.textContent = 12 + countedMonth
+		$yearResult.textContent = countedYear - 1
 	} else if (countedMonth < 0) {
-		$yearResult.textContent = `${countedYear - 1}`
-		$monthResult.textContent = `${12 - -countedMonth}`
-		$dayResult.textContent = `${countedDays}`
+		$monthResult.textContent = 12 + countedMonth
+		$dayResult.textContent = countedDays
+		$yearResult.textContent = countedYear - 1
 	} else if (countedDays < 0) {
-		$yearResult.textContent = `${countedYear}`
-		$monthResult.textContent = `${countedMonth - 1}`
-		$dayResult.textContent = `${daysInMonth($monthInput.value, $yearInput.value) - -countedDays}` 
-	} 
+		$dayResult.textContent = daysInMonth($monthInput.value, $yearInput.value) + countedDays
+		$monthResult.textContent = countedMonth - 1
+		$yearResult.textContent = countedYear
+	} else {
+		$dayResult.textContent = countedDays
+		$monthResult.textContent = countedMonth
+		$yearResult.textContent = countedYear
+	}
 
-	console.log($currentMonth);
+	console.log(countedDays)
 }
 
 window.addEventListener('DOMContentLoaded', main)
